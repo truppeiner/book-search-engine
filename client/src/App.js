28ad7f704f2a35ef0fef2affc74ebaedbb0d8b52
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-19 17:58:46 -0500

    added apollo client connection to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,48 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// link apollo client start
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from, } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
+
+// will log error if client cannot connect to graphql successfully
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors){
+    graphqlErrors.map(({ message, location, path }) => {
+      return alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+// authorization
+const authLink = setContext((_, { headers }) =>{
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// link backend
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:3001/graphql" }),
+]);
+
+// start client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(link)
+})
+
+// end apollo client link 
 function App() {
   return (
+    // link apollo client to graphql api
+    <ApolloProvider client = {client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +56,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
