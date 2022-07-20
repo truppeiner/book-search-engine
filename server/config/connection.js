3c3fc2f31fdc952c4b9fa115811704a721a726a7
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-19 19:15:43 -0500

    deploy attempt 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/glacial-stream-45568', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Book-Search-Engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
